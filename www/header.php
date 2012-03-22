8bba05f46065c1c1f2efc2d7a299f811f2b4e6d5
Justin Barlow <jcbarlow@xactware.com>
2012-03-22 12:01:17 -0600

    Added documentation to the header.php file

--- www/header.php
+++ www/header.php
@@ -1,20 +1,30 @@
 <?php
-
+// Load our config file
 require_once(realpath(dirname(__FILE__).'/../config/config.inc.php'));
+
+// Load and start our PageTimerService
 require_once(NLB_LIB_ROOT.'PageTimerService.class.php');
 $PageTimer = new PageTimerService();
 $PageTimer->start();
 
+// Turn error reporting on/off
 if(SHOW_ERRORS)
 {
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 }
+else
+{
+	error_reporting(0);
+	ini_set('display_errors', 0);
+}
 
+// Load classes that will be needed on almost every page
 require_once(NLB_LIB_ROOT.'DatabaseService.class.php');
 require_once(NLB_LIB_ROOT.'UIService.class.php');
 require_once(NLB_LIB_ROOT.'User.class.php');
 
+// get an instance of the DatabaseService to communicate/use the database
 try
 {
 	$DB = DatabaseService::getInstance();
@@ -28,4 +38,5 @@ catch(DatabaseServiceException $e)
 	}
 }
 
+// get an instance of the UIService to be able to render UI components
 $UI = UIService::getInstance();
